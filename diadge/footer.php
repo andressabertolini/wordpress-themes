1c7541b4c00339601798fa9204dea66d428c117f
Andressa <andressabtl@gmail.com>
2016-03-08 20:35:00 +0100

    Structure

---
+++ diadge/footer.php
@@ -0,0 +1,11 @@
+        <div class="footer" style="background: <?php echo get_theme_mod('footer_color'); ?>"></div>
+        
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+
+        
+        <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+        <?php wp_footer(); ?>
+
+    </body>
+</html>
---
+++ diadge/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+        <title><?php wp_title(); ?></title>
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+
+        <?php wp_head(); ?>
+
+</head>
+<body>

fa7d2d8a20767537a702186185c2d7ca01777264
Andressa <andressabtl@gmail.com>
2016-03-17 15:33:00 +0100

    Index

---
+++ diadge/index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<!-- ******************************************************** -->
+<!-- CONTENT -->
+
+<div class="wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-8">
+                <div class="posts">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>

ee79cff1951ce9f178537874e665d6de7cc54736
Andressa <andressabtl@gmail.com>
2016-04-17 11:15:00 +0100

    Sidebar

---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<!-- ******************************************************** -->
+<!-- ABOUT -->
+<div class="col-sm-12 col-md-4">
+                        <div class="about">
+                            <div class="sidebar-author sidebar-session">
+                                <div class="about-image-corners">
+                                <?php 
+                                    $empty = get_theme_mod('author_image');
+
+                                    if($empty != ''){
+                                        echo '
+                                        <img src="'. get_theme_mod('author_image') .'" class="about-image" />
+                                        <div class="corner-top-left"></div>
+                                        <div class="corner-top-right"></div>
+                                        <div class="corner-bottom-left"></div>
+                                        <div class="corner-bottom-right"></div>
+                                        ';
+                                    }
+                                ?>
+
+                                </div>   
+
+                                <h1 class="about-title" style="color: <?php echo get_theme_mod('sidebar_title_color','#'); ?>"><?php echo get_theme_mod('author_name','Author'); ?></h1>
+
+                                <p class="about-text"><?php echo get_theme_mod('author_description','Lorem ipsum dolor sit amet.'); ?></p>
+
+                            </div> 
+
+                            <!-- Widgets area -->
+                            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
